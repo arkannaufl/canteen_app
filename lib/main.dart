a32e57a94ff61b24364d3d94cb49120a731f2c11
arkannaufl <arkannaufal024@gmail.com>
2024-11-16 06:22:23 +0700

    Deskripsi perubahan

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:canteen_app/data/dataresource/auth_local_datasource.dart';
 import 'package:canteen_app/presentation/auth/blocs/login/login_bloc.dart';
+import 'package:canteen_app/presentation/general/dashboard_page.dart';
 import 'package:canteen_app/presentation/general/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -28,7 +30,16 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
           useMaterial3: true,
         ),
-        home: const SplashPage(),
+        home: FutureBuilder(
+            future: AuthLocalSource().isAuthData(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData && snapshot.data == true) {
+                return const DashboardPage();
+              } else {
+              return const SplashPage();
+              }
+            }
+        ),
       ),
     );
   }
